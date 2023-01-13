1b73a54cc9e69d8058dee6a8b16067f136bb9934
avi96_rc <avi96.rc@gmail.com>
2023-01-14 02:41:26 +0530

    ADD: header

--- App.js
+++ App.js
@@ -1,23 +1,66 @@
 import React from "react";
-import { createElement } from "react";
 import ReactDOM from "react-dom/client";
 
-const Title =  (
-  <h1 id="title" key="h2">
-    this is title
-  </h1>
+const Title = () => (
+  <a href="/">
+    <img
+      class="logo"
+      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+      alt=""
+    />
+  </a>
 );
 
+/**
+     Header
+        -logo
+        -list items(right side)
+        -cart
+
+     body
+        -search bar
+        -resturant list
+            -image
+            -name
+            -rating
+            -cuisines
+     Footer       
+     */
+
 const HeaderComponent = () => {
+ 
   return (
-    <div>
-        {Title}
-      <h1>Namaste react functional component</h1>
-      <h2>this heading 2</h2>
+    <div className="header" style={{boxShadow:'3px 3px 15px 3px rgb(204, 203, 203)'}}>
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
+const Body = () => {
+  returnv(<h4>Body</h4>);
+};
+const Footer = () => {
+  returnv(<h4>Footer</h4>);
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent/>);
+root.render(<HeaderComponent />);
 console.log("amit");
